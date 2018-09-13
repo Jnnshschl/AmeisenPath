42e64b0be26d1bd0328a45b01343ddd7008d526e
Jnnshschl <jannis@jnns.de>
2018-09-13 18:03:22 +0200

    added Z position

--- AmeisenPathLib/objects/NodePosition.cs
+++ AmeisenPathLib/objects/NodePosition.cs
@@ -4,11 +4,13 @@
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public int Z { get; set; }
 
-        public NodePosition(int x, int y)
+        public NodePosition(int x, int y, int z)
         {
             X = x;
             Y = y;
+            Z = z;
         }
     }
 }
