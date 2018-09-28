0342dc0b219ca6b1a521a4c4c842c2074bccc439
Jnnshschl <jannis@jnns.de>
2018-09-28 18:23:10 +0200

    Heavy GUI-Drawing optimization

--- AmeisenPath.GUI/MainWindow.xaml.cs
+++ AmeisenPath.GUI/MainWindow.xaml.cs
@@ -1,29 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
-using System.IO;
-using AmeisenPathCore;
+﻿using AmeisenPathCore;
 using AmeisenPathCore.Objects;
-using System.Diagnostics;
 using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
 using System.Windows.Media.Imaging;
 
 namespace AmeisenPathGUI
 {
     public partial class MainWindow : Window
     {
-        NodePosition startPos;
-        NodePosition endPos;
-
-        List<Node> pathToGo = new List<Node>();
-
-        Node[,] map;
+        private NodePosition startPos;
+        private NodePosition endPos;
+        private List<Node> pathToGo = new List<Node>();
+        private Node[,] map;
 
         // Tile size, maybe a bit hacky but hey it works :D
         private int tileSize = 5;
+
+        private Brush blockedBrush = new SolidBrush(Color.FromArgb(255, 77, 77, 77));
+        private Brush startBrush = new SolidBrush(Color.Lime);
+        private Brush endBrush = new SolidBrush(Color.Red);
+        private Brush orangeBrush = new SolidBrush(Color.Orange);
+        private Brush blackBrush = new SolidBrush(Color.FromArgb(255, 50, 50, 50));
+        private Brush selectedBrush = null;
 
         public MainWindow()
         {
@@ -37,12 +41,19 @@ namespace AmeisenPathGUI
 
         private void ResetMap()
         {
+            pathImage.Visibility = Visibility.Hidden;
+            mapImage.Visibility = Visibility.Hidden;
+
             Random rnd = new Random();
-            startPos = new NodePosition(rnd.Next(0, (int)canvasPath.ActualWidth / tileSize), rnd.Next(0, (int)canvasPath.ActualHeight / tileSize));
-            endPos = new NodePosition(rnd.Next(0, (int)canvasPath.ActualWidth / tileSize), rnd.Next(0, (int)canvasPath.ActualHeight / tileSize));
+            //startPos = new NodePosition(rnd.Next(0, (int)mapCanvas.ActualWidth / tileSize), rnd.Next(0, (int)mapCanvas.ActualHeight / tileSize));
+            //endPos = new NodePosition(rnd.Next(0, (int)mapCanvas.ActualWidth / tileSize), rnd.Next(0, (int)mapCanvas.ActualHeight / tileSize));
+
+            startPos = new NodePosition(0, 0);
+            endPos = new NodePosition((int)mapCanvas.ActualWidth / tileSize - 1, (int)mapCanvas.ActualHeight / tileSize - 1);
+
             pathToGo = new List<Node>();
 
-            map = new Node[(int)canvasPath.ActualWidth / tileSize, (int)canvasPath.ActualHeight / tileSize];
+            map = new Node[(int)mapCanvas.ActualWidth / tileSize, (int)mapCanvas.ActualHeight / tileSize];
 
             Stopwatch sw = new Stopwatch();
             sw.Start();
@@ -53,66 +64,11 @@ namespace AmeisenPathGUI
 
             sw.Reset();
             sw.Start();
-            DrawMap();
+            RenderMap();
             sw.Stop();
 
             labelTimeGridGen.Content = $"MapGeneration: {generationmillis}ms\nMapDrawing: {sw.ElapsedMilliseconds}ms";
-        }
-
-        private void DrawMap()
-        {
-            SolidColorBrush blockedBrush = new SolidColorBrush(Color.FromArgb(255, 50, 50, 50));
-            SolidColorBrush startBrush = new SolidColorBrush(Colors.Lime);
-            SolidColorBrush endBrush = new SolidColorBrush(Colors.Red);
-            SolidColorBrush selectedBrush = null;
-
-            canvasPath.Children.Clear();
-
-            DrawingVisual dv = new DrawingVisual();
-            using (DrawingContext dc = dv.RenderOpen())
-            {
-                for (int x = 0; x < map.GetLength(0); x++)
-                    for (int y = 0; y < map.GetLength(1); y++)
-                    {
-                        if (map[x, y].IsBlocked)
-                            selectedBrush = blockedBrush;
-                        else if (x == endPos.X && y == endPos.Y)
-                            selectedBrush = endBrush;
-                        else if (x == startPos.X && y == startPos.Y)
-                            selectedBrush = startBrush;
-                        else continue;
-
-                        if (checkboxDrawCosts.IsChecked == true)
-                        {
-                            if (map[x, y].GCost != 0)
-                                DrawText((tileSize * x) + 4, (tileSize * y), "G: " + map[x, y].GCost, Colors.White, tileSize / 3);
-                            if (map[x, y].HCost != 0)
-                                DrawText((tileSize * x) + 4, (tileSize * y) + 30, "H: " + map[x, y].HCost, Colors.White, tileSize / 3);
-                            if (map[x, y].FCost != 0)
-                                DrawText((tileSize * x) + 4, (tileSize * y) + 14, "F: " + map[x, y].FCost, Colors.White, tileSize / 3);
-                        }
-
-                        dc.DrawRectangle(
-                            selectedBrush,
-                            null,
-                            new Rect((tileSize * x), (tileSize * y), tileSize, tileSize));
-                        selectedBrush = null;
-                    }
-                canvasPath.Children.Add(new VisualHost { Visual = dv });
-            }
-        }
-
-        private void DrawText(double x, double y, string text, Color color, double fontSize)
-        {
-            TextBlock textBlock = new TextBlock
-            {
-                Text = text,
-                FontSize = (int)fontSize,
-                Foreground = new SolidColorBrush(color)
-            };
-            Canvas.SetLeft(textBlock, x);
-            Canvas.SetTop(textBlock, y);
-            canvasPath.Children.Add(textBlock);
+            labelMapInfo.Content = $"Map Width: {(int)mapCanvas.ActualWidth / tileSize}\nMap Height: {(int)mapCanvas.ActualHeight / tileSize}\nDistance: {(int)Math.Sqrt(Math.Pow(startPos.X - endPos.X, 2) + Math.Pow(startPos.Y - endPos.Y, 2))}";
         }
 
         private void FillMap()
@@ -124,8 +80,15 @@ namespace AmeisenPathGUI
                 {
                     bool blockIt = rnd.Next(0, 4) == 3 ? true : false;
 
-                    if (x == startPos.X && y == startPos.Y) blockIt = false;
-                    if (x == endPos.X && y == endPos.Y) blockIt = false;
+                    if (x == startPos.X && y == startPos.Y)
+                    {
+                        blockIt = false;
+                    }
+
+                    if (x == endPos.X && y == endPos.Y)
+                    {
+                        blockIt = false;
+                    }
 
                     map[x, y] = new Node(new NodePosition(x, y), blockIt);
                 }
@@ -143,38 +106,153 @@ namespace AmeisenPathGUI
 
             sw.Reset();
             sw.Start();
-            DrawPath();
+            RenderPath();
             sw.Stop();
 
             labelTimePathGen.Content = $"PathFinding: {pathfindingmillis}ms\nMapDrawing: {sw.ElapsedMilliseconds}ms";
         }
 
-        private void DrawPath()
+        private void RenderMap()
         {
-            SolidColorBrush orangeBrush = new SolidColorBrush(Colors.Orange);
-
-            DrawingVisual dv = new DrawingVisual();
-            using (DrawingContext dc = dv.RenderOpen())
+            Bitmap bitmap = new Bitmap(map.GetLength(0) * tileSize, map.GetLength(1) * tileSize);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
             {
-                for (int x = 0; x < map.GetLength(0); x++)
-                    for (int y = 0; y < map.GetLength(1); y++)
-                    {                        
-                        if (pathToGo != null)
+                RenderMap(graphics);
+            }
+
+            BitmapImage bitmapImageMap = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImageMap.BeginInit();
+                bitmapImageMap.StreamSource = memory;
+                bitmapImageMap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImageMap.EndInit();
+            }
+
+            mapImage.Source = bitmapImageMap;
+            mapImage.Visibility = Visibility.Visible;
+        }
+
+        private void RenderPath()
+        {
+            Bitmap bitmap = new Bitmap(map.GetLength(0) * tileSize, map.GetLength(1) * tileSize);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                RenderPath(graphics);
+            }
+
+            BitmapImage bitmapImagePath = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImagePath.BeginInit();
+                bitmapImagePath.StreamSource = memory;
+                bitmapImagePath.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImagePath.EndInit();
+            }
+
+            pathImage.Source = bitmapImagePath;
+            pathImage.Visibility = Visibility.Visible;
+        }
+
+        private void RenderMap(Graphics graphics)
+        {
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    if (map[x, y].IsBlocked)
+                    {
+                        selectedBrush = blockedBrush;
+                    }
+                    else if (x == endPos.X && y == endPos.Y)
+                    {
+                        selectedBrush = endBrush;
+                    }
+                    else if (x == startPos.X && y == startPos.Y)
+                    {
+                        selectedBrush = startBrush;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                    graphics.FillRectangle(
+                        selectedBrush,
+                        new Rectangle(tileSize * x, tileSize * y, tileSize, tileSize));
+                }
+            }
+        }
+
+        private void RenderPath(Graphics graphics)
+        {
+            if (pathToGo != null)
+            {
+                int stepcount = 0;
+                foreach (Node n in pathToGo)
+                {
+                    int x = n.Position.X;
+                    int y = n.Position.Y;
+
+                    if (endPos.X == n.Position.X && endPos.Y == n.Position.Y)
+                    {
+                        selectedBrush = endBrush;
+                    }
+                    else
+                    {
+                        selectedBrush = orangeBrush;
+                    }
+
+                    if (x == n.Position.X && y == n.Position.Y)
+                    {
+                        x *= tileSize;
+                        y *= tileSize;
+
+                        graphics.FillRectangle(
+                            selectedBrush,
+                            new Rectangle(x, y, tileSize, tileSize));
+                        // Draw stepcount only if its visible
+                        if (tileSize > 20 && checkboxDrawCosts.IsChecked == false)
                         {
-                            foreach (Node n in pathToGo)
-                            {
-                                if (x == n.Position.X && y == n.Position.Y)
-                                {
-                                    dc.DrawRectangle(
-                                        orangeBrush,
-                                        null,
-                                        new Rect((tileSize * x), (tileSize * y), tileSize, tileSize));
-                                    break;
-                                }
-                            }
+                            graphics.DrawString(
+                            $"{stepcount}",
+                            new Font(System.Drawing.FontFamily.GenericSansSerif, tileSize / 3, System.Drawing.FontStyle.Regular),
+                            blackBrush,
+                            x,
+                            y);
+                        }
+
+                        // Draw costs only if its visible
+                        if (tileSize > 20 && checkboxDrawCosts.IsChecked == true)
+                        {
+                            graphics.DrawString(
+                            $"F: {n.FCost}",
+                            new Font(System.Drawing.FontFamily.GenericSansSerif, tileSize / 4, System.Drawing.FontStyle.Regular),
+                            blackBrush,
+                            x,
+                            y + ((tileSize / 4) * 0));
+
+                            graphics.DrawString(
+                            $"G: {n.GCost}",
+                            new Font(System.Drawing.FontFamily.GenericSansSerif, tileSize / 4, System.Drawing.FontStyle.Regular),
+                            blackBrush,
+                            x,
+                            y + ((tileSize / 4) * 1) + 2);
+
+                            graphics.DrawString(
+                            $"H: {n.HCost}",
+                            new Font(System.Drawing.FontFamily.GenericSansSerif, tileSize / 4, System.Drawing.FontStyle.Regular),
+                            blackBrush,
+                            x,
+                            y + ((tileSize / 4) * 2) + 4);
                         }
                     }
-                canvasPath.Children.Add(new VisualHost { Visual = dv });
+                    stepcount++;
+                }
             }
         }
 
